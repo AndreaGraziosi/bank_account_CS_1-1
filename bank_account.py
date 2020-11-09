82ef61440361c9f25b9c866e0eb6eb694ca894dc
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-11-09 12:24:28 -0800

    created objects

--- bank_account.py
+++ bank_account.py
@@ -1,9 +1,14 @@
+import random
+account_numbers = list(range(10000000,100000000))
+
 class BankAccount:
-    def __init__(self, full_name, account_number, routing_number, balance):
+    routing_number = 123456789
+    def __init__(self, full_name, account_number):
         self.full_name = full_name
-        self.account_number = account_number
-        self.routing_number = routing_number
-        self.balance = balance
+        random.shuffle(account_numbers)
+        self.account_number = account_numbers.pop(len(account_number))
+        self.balance = 0
+
 
     def deposit(self, amount):
         """ add amount to balance"""
@@ -22,22 +27,32 @@ class BankAccount:
         
        
 
-    def get_balance():
+    def get_balance(self):
         """print a friendly balance message and return account balance"""
-        #self.deposit(50)
-       # print ("Your Balance is:")
-       # pass
+        print ("your account balance is: ", self.balance)
+        return self.balance
+        
 
-   # def get_interest():
-        """adds interest to users Balance"""
-        #monthly_interest= balance * 0.00083
+       
+    def monthly_interest(self):
+        """helper function calculating interest"""
+        return self.balance * 0.00083
+        
 
-   # def print_receipt():
-        """prints a receipt with name account number and balance"""
+    def add_interest(self):
+        """adds monthly interest to the balance"""
+        self.balance += self.monthly_interest()
+        
+    def print_receipt(self):
+        """print the receipt with Full Name, Accountnumber routing number and Balance"""
+        print ('{} {} {}'.format(self.full_name, self.account_number, self.balance))
+        
+ 
+account = BankAccount("andreaGraziosi", 200)
+account.deposit(35)
+account.withdraw(15)
+account.balance()
 
-'{} {}'.format('one', 'two') 
-#define 3 different bank account examples using the BankAccount() object. 
-    
-account = BankAccount("andrea Graziosi", "25872487234873", "462984", 200)
-account.deposit(10)
-account.withdraw(215)
+account2 = BankAccount("leaSmith",300)
+account3 = BankAccount("Thais DaSilva", 45)
+print(account.print_receipt())
---
+++ test.py
@@ -0,0 +1,5 @@
+def multiply_by_two(num1, num2):
+    result = num1 * num2
+    print(result)
+
+multiply_by_two(4, 2)
