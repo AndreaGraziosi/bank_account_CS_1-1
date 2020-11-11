a49d3e1e9ae3400e3839510bc19cb6aa1ddd24d8
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-11-10 18:33:56 -0800

    corrections and added sensitizing method

--- bank_account.py
+++ bank_account.py
@@ -1,8 +1,8 @@
 import random
 account_number = list(range(10000000,100000000))
-print("account lists are made")
+print("account numbers have been generated")
 random.shuffle(account_number)
-
+print("numbers have been shuffled")
 class BankAccount:
     routing_number = 123456789
     def __init__(self, full_name):
@@ -10,13 +10,14 @@ class BankAccount:
         self.account_number = account_number.pop(len(account_number)-1)
         self.balance = 0
 
-
+    def sensitize_account_number(self):
+        return "****" + str(self.account_number)[4:]
 
     def deposit(self, amount):
         """ add amount to balance"""
         self.balance += amount
         print ("Amount Deposited: $", amount)
-        print (self.balance)
+        
 
     def withdraw(self, amount):
         """subtract from balance and overdraft"""
@@ -31,7 +32,7 @@ class BankAccount:
 
     def get_balance(self):
         """print a friendly balance message and return account balance"""
-        print ("your account balance is: ", self.balance)
+        print ("your account balance is: $", self.balance)
         return self.balance
         
 
@@ -45,29 +46,36 @@ class BankAccount:
         """adds monthly interest to the balance"""
         self.balance += self.monthly_interest()
         
-    def print_receipt(self):
+    
+
+    def print_receipt2(self):
         """print the receipt with Full Name, Accountnumber routing number and Balance"""
-        print ('{}\n Account No.:{}\n Routing No.:{}\n Balance:{}'.format(self.full_name, self.account_number, self.routing_number, self.balance))
+        print ('{}\n Account No.:{}\n Routing No.:{}\n Balance: ${}'.format(self.full_name, self.sensitize_account_number(), self.routing_number, self.balance))
         
-print("account list made")
-account = BankAccount("andreaGraziosi")
-print(account.account_number)
+
+account = BankAccount("Andrea-Graziosi")
+
 account.deposit(55)
 account.withdraw(5)
 account.get_balance()
 account.add_interest()
-account.print_receipt()
 print("---------------------------")
-account2 = BankAccount("leaSmith")
+account.print_receipt2()
+print("---------------------------")
+account2 = BankAccount("Lea-Smith")
 account2.deposit(600)
 account2.withdraw(300)
 account2.get_balance()
 account2.add_interest()
-account2.print_receipt()
 print("---------------------------")
-account3 = BankAccount("Thais DaSilva")
+account2.print_receipt2()
+print("---------------------------")
+account3 = BankAccount("Thais-DaSilva")
 account3.deposit(200)
 account3.withdraw(400)
 account3.get_balance()
 account3.add_interest()
-account3.print_receipt()
+print("---------------------------")
+account3.print_receipt2()
+
+
